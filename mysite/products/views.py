f537dd7c22fc52c60c186b3e4442cf0f6c38fd62
francis0cheng <francis000cheng@gmail.com>
2019-04-14 16:01:37 +0800

    f

--- mysite/products/views.py
+++ mysite/products/views.py
@@ -1,22 +1,42 @@
 from django.shortcuts import render
 
 from .models import Product
-from .froms import ProductForm
+from .froms import ProductForm, RawProductForm
 
 
 # Create your views here.
 
-def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
 
-        # rerender it, to clear the text
-        form = ProductForm()
+def product_create_view(request):
+    my_form = RawProductForm()
+    if request.method == 'POST':
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            # now the data is good
+            print(my_form.cleaned_data)
+            # create new obj with cleaned_data
+            Product.objects.create(**my_form.cleaned_data)
+            # the ** turns my_form.cleaned_data into args
+        else:
+            print(my_form.errors)
     context = {
-        'form': form
+        'form': my_form,
     }
-    return render(request, "products/product_create.html", context)
+
+    return render(request, 'products/product_create.html', context)
+
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#
+#         # rerender it, to clear the text
+#         form = ProductForm()
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
